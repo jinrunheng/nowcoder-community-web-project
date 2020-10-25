cd9eb0934ad1627e00687725a96ffad8522e7d14
macbook <macbook@macdeMacBook-Pro.local>
2020-10-25 16:24:24 +0800

    add spring mvc code example

--- community/src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ community/src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -3,8 +3,15 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.service.AlphaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
 
 @Controller
 @RequestMapping("/alpha")
@@ -23,5 +30,116 @@ public class AlphaController {
     @ResponseBody
     public String sayHello() {
         return "Hello Spring Boot";
+    }
+
+    @RequestMapping("/http")
+    public void http(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // request method
+        System.out.println(request.getMethod());
+        // request path
+        System.out.println(request.getServletPath());
+
+        // headers
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + " : " + value);
+        }
+        // parameter
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+            String parameterKey = entry.getKey();
+            String[] parameterValues = entry.getValue();
+            for (String parameterValue : parameterValues) {
+                System.out.println(parameterKey + " : " + parameterValue);
+            }
+
+        }
+
+        // response
+        response.setContentType("text/html;charset-utf-8");
+        // try with resources
+        try (PrintWriter writer = response.getWriter()) {
+            writer.write("<h1>Hello HTTP</h1>");
+        }
+    }
+
+    // GET request
+    // path: /students?page=1&limit=20
+    // @RequestMapping(path = "/student",method = RequestMethod.GET)
+    @GetMapping(path = "/students")
+    @ResponseBody
+    public String getStudents(
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) {
+        System.out.println(page);
+        System.out.println(limit);
+        return "some students";
+    }
+
+    // /student/123
+    @RequestMapping(path = "/student/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudentById(@PathVariable("id") int id) {
+        System.out.println(id);
+        return "a student";
+    }
+
+    // POST request
+    @PostMapping(path = "/student")
+    @ResponseBody
+    public String saveStudent(@RequestParam(name = "name") String name,
+                              @RequestParam(name = "age") int age) {
+        System.out.println(name);
+        System.out.println(age);
+        return "success";
+    }
+
+    // 响应HTML数据
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public ModelAndView getTeacher() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name", "张三");
+        modelAndView.addObject("age", 30);
+        modelAndView.setViewName("/demo/view");
+        return modelAndView;
+    }
+
+    @RequestMapping(path = "/school", method = RequestMethod.GET)
+    public String getSchool(Model model) {
+        model.addAttribute("name", "北京大学");
+        model.addAttribute("age", 80);
+        return "/demo/view";
+    }
+
+    // 响应 JSON（异步请求）
+    @RequestMapping(path = "/emp", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getEmp() {
+        Map<String, Object> emp = new HashMap<>();
+        emp.put("name", "张三");
+        emp.put("age", 23);
+        emp.put("salary", 8000);
+        return emp;
+    }
+
+    @RequestMapping(path = "/emps", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Map<String, Object>> getEmps() {
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        Map<String, Object> emp1 = new HashMap<>();
+        emp1.put("name", "张三");
+        emp1.put("age", 23);
+        emp1.put("salary", 8000);
+        list.add(emp1);
+
+        Map<String, Object> emp2 = new HashMap<>();
+        emp2.put("name", "李四");
+        emp2.put("age", 29);
+        emp2.put("salary", 10000);
+        list.add(emp2);
+        return list;
     }
 }
--- note.md
+++ note.md
@@ -90,4 +90,90 @@
 - IoC Container
   - IoC容器，是实现依赖注入的关键，本质上是一个工厂
 
- 
+ ## 三：Spring MVC入门
+
+#### HTTP
+
+HTTP：Hyper Text Transfer Protocol
+
+#### HTTP流
+
+客户端与服务端进行交互时，过程表现为以下几个步骤
+
+1. 打开一个TCP连接
+
+2. 发送一个HTTP报文(HTTP Request)
+
+   HTTP请求信息由三部分组成：
+
+   1. method  Path  version of the protocol
+   2. 请求头（Request Header）
+   3. 请求正文
+
+   ```http
+   GET / HTTP/1.1
+   Accept:image/gif.image/jpeg,*/*
+   Accept-Language:zh-cn
+   Connection:Keep-Alive
+   Host:localhost
+   User-Agent:Mozila/4.0(compatible;MSIE5.01;Window NT5.0)
+   Accept-Encoding:gzip,deflate
+   
+   username=root&password=1234
+   ```
+
+3. 接收服务端返回的报文信息（HTTP Response）
+
+   HTTP响应信息由三部分组成
+
+   1. Version of protocol / status code /status message
+   2. 响应头（Response Header）
+   3. 响应正文
+
+   ```http
+   HTTP/1.1 200 OK
+   Server:Apache Tomcat/5.0.12
+   Date:Mon,6Oct2003 13:23:42 GMT
+   Content-Length:112
+    
+      <html>
+        <head>
+        <title>HTTP响应示例<title>
+    </head>
+    <body>
+         Hello HTTP!
+    </body>
+   </html>
+   ```
+
+4. 关闭连接或者为后续请求重用连接
+
+#### Spring MVC
+
+- 三层架构
+  - 表现层，业务层，数据访问层
+
+- MVC（表现层）
+  - Model
+  - View
+  - Controller
+- 核心组件
+  - 前端控制器：DispatcherServlet
+- Spring MVC 的执行流程
+  1. 用户浏览器发送请求至前端控制器DispatcherServlet
+  2. DispatcherServlet收到请求后调用HandlerMapping处理器映射器
+  3. HandlerMapping根据请求url找到具体的处理器，生成处理器执行链(HandlerExecutionChain)包括处理器对象以及处理器拦截器(有则生成),一并返回给DipatcherServlet
+  4. DispatcherServlet通过HandlerAdapter(处理器适配器)调用处理器，执行处理器
+  5. 处理器执行完成后返回ModelAndView，并返回给HandlerAdapter，HandlerAdapter将结果返回给DispatcherServlet
+  6. DispatcherServlet将ModelAndView传给ViewResolver视图解析器，ViewResolver解析后返回具体的View给DispatcherServlet
+  7. DispatcherServlet对View进行渲染(即将模型数据填充至视图中)后返回给客户端口
+
+#### Thymeleaf
+
+- 模版引擎
+  - 生成动态的HTML
+- Thymeleaf
+  - 倡导自然模版，即以HTML文件为模版
+- 常用语法
+  - 标准表达式，判断与循环，模版的布局
+
